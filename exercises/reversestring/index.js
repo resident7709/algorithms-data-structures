633b7977df4a6b285be74822210123cbe7c2289c
resident7709 <resident7709@gmail.com>
2023-06-10 23:31:00 +0300

    second commit

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,32 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// * Built-in Function + Chaining Methods
+// function reverse(str) {
+//   return str.split("").reverse("").join("");
+// }
+
+// * for (...of) Loop
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// * Recursion + Ternary Operator
+function reverse(str) {
+  return str === "" ? "" : reverse(str.substr(1)) + str.charAt(0);
+}
+
+reverse("apple");
+
+module.exports = reverse;
